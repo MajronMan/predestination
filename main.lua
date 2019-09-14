e3965c6d9ec36d566880a3f3758e89dcc1454942
Miron Markowski <miron.markowski@bjss.com>
2019-09-14 15:09:25 +0100

    Add some libraries

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ local Gui = require("gui.Gui")
 local GameState = require("state.GameState")
 local map = require("assets.map")
 
+local nuklear = require("nuklear")
+
 function love.load()
     -- CR. why dofile instead of require when it's .lua anyway?
     -- BTW we should serialize state to some other place
@@ -11,6 +13,8 @@ function love.load()
     font = love.graphics.newFont(data.fontSize)
     love.graphics.setFont(font)
     state = GameState:new(data)
+    love.keyboard.setKeyRepeat(true)
+    ui = nuklear.newUI()
 end
 
 function love.update(dt)
@@ -29,10 +33,36 @@ function love.draw()
     state.player:draw()
 
     Gui.draw(state.player:getStats())
+    ui:draw()
 end
 
 function love.keypressed(key, scancode, isrepeat)
     if key == "escape" then
         love.event.quit()
     end
+    ui:keypressed(key, scancode, isrepeat)
+end
+
+function love.keyreleased(key, scancode)
+    ui:keyreleased(key, scancode)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    ui:mousepressed(x, y, button, istouch, presses)
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    ui:mousereleased(x, y, button, istouch, presses)
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    ui:mousemoved(x, y, dx, dy, istouch)
+end
+
+function love.textinput(text)
+    ui:textinput(text)
+end
+
+function love.wheelmoved(x, y)
+    ui:wheelmoved(x, y)
 end
