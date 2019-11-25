6fe5278a4febcd76c635c746a9af3f5ab65cadc9
luknw <luknwpl@gmail.com>
2019-11-25 17:33:51 +0100

    Clean up spell assets

--- assets/initialData.lua
+++ assets/initialData.lua
@@ -5,15 +5,8 @@ local spellbook = {}
 table.insert(
     spellbook,
     {
-        width = 32,
-        height = 32,
-        image = "assets/images/player.png",
-        xOffset = 0,
-        yOffset = 16,
-        x = 100,
-        y = 100,
-        name = "Heal",
         mp = 10,
+        name = "Heal",
         effect = function(target)
             target.hp = target.hp + 10
         end
@@ -23,15 +16,8 @@ table.insert(
 table.insert(
     spellbook,
     {
-        x = 50,
-        y = 50,
-        width = 32,
-        height = 32,
-        image = "assets/images/player.png",
-        xOffset = 0,
-        yOffset = 16,
-        name = "Restore Mana",
         mp = 0,
+        name = "Restore Mana",
         effect = function(target)
             target.mp = target.mp + 10
         end
