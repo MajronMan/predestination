ab1d5853cee004fc9f0ce73f9258068c4031fb66
luknw <luknwpl@gmail.com>
2019-12-09 16:24:50 +0100

    Add width and height introspection to Group

--- ui/Group.lua
+++ ui/Group.lua
@@ -7,11 +7,13 @@ function Group:initialize(window, title, rBounds, flags)
     self._title = title
     self._rBounds = rBounds
     self._flags = flags or {}
+    self._x, self._y, self._w, self._h = nil, nil, nil, nil
 end
 
 function Group:frame(ui)
     ui:layoutSpacePush(unpack(self._rBounds))
     if ui:groupBegin(self._title, unpack(self._flags)) then
+        self._x, self._y, self._w, self._h = ui:layoutSpaceBounds()
         self:body(ui)
         ui:groupEnd()
     end
@@ -19,6 +21,14 @@ end
 
 function Group:body(ui)
     error(self.class.name .. ": Group body undefined")
+end
+
+function Group:w(ratio)
+    return self._w * (ratio or 1)
+end
+
+function Group:h(ratio)
+    return self._w * (ratio or 1)
 end
 
 return Group
